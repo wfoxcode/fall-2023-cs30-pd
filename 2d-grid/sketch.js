eb973bcaa5f7e8b3d23289317b1d02a03c824d94
wfoxcode <147782599+wfoxcode@users.noreply.github.com>
2023-10-12 20:56:02 -0600

    ex2

--- 2d-grid/sketch.js
+++ 2d-grid/sketch.js
@@ -1,10 +1,10 @@
 // 2D Grid
 // Oct 12, 2023
 
-let grid = [[0, 1, 0, 0],
-            [1, 0, 1, 1],
-            [0, 0, 1, 1],
-            [1, 1, 0, 1]];
+// let grid = [[0, 1, 0, 0],
+//             [1, 0, 1, 1],
+//             [0, 0, 1, 1],
+//             [1, 1, 0, 1]];
 
 const ROWS = 4;
 const COLS = 4;
@@ -12,6 +12,8 @@ let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  let grid = createRandomGrid(ROWS,COLS);
 
   if (width < height) {
     cellSize = width/ROWS;
@@ -51,5 +53,41 @@ function displayGrid() {
       }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
+  }
+}
+
+function createRandomGrid(ROWS,COLS) {
+  let newGrid = [];
+  for (let y=0; y < ROWS; y++) {
+    newGrid.push([]);
+    for (let x=0; x<COLS; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(1);
+      }
+      else {
+        newGrid[y].push(0);
+      }
+    }
+  }
+
+  return newGrid;
+}
+
+function createEmptyGrid {
+  let newGrid = [];
+  for (let y=0; y < ROWS; y++) {
+    newGrid.push([]);
+    for (let x=0; x<COLS; x++) {
+      newGrid[y].push([]);
+    }
+  }
+}
+
+function keyTyped() {
+  if (key==="r") {
+    grid = createRandomGrid(ROWS,COLS);
+  }
+  else if (key==="e") {
+    grid = createEmptyGrid(ROWS,COLS);
   }
 }
